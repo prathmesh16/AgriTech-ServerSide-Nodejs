4e86e2433973d9d3162ec2896de657522d4fcbf2
prathmesh16 <40669323+prathmesh16@users.noreply.github.com>
2021-06-05 23:34:55 +0530

    Added CORS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 
 const userRouter = require('./routers/user_router')
 
 const app = express()
 const port = process.env.PORT
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
  
